11bfbee3528592d372c4007a9d0d6d6efa2b4097
An980 <9807594@mail.ru>
2022-09-06 20:11:58 +0300

    TestNG added

---
+++ src/test/java/ru/netology/service/test/CashbackHackServiceTest.java
@@ -0,0 +1,50 @@
+package ru.netology.service.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.netology.service.CashbackHackService;
+
+public class CashbackHackServiceTest {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+    public void purchaseBelowThousand () {
+
+        int expected = 500;
+        int actual = service.remain(500);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+    @Test
+    public void purchaseAboveThousand () {
+
+        int expected = 500;
+        int actual = service.remain(1500);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void equalPurchase () {
+
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @Test
+    public void zeroPurchase() {
+
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+}
